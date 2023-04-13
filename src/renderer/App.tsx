4e60733b5ec558f939ae512d4d95b8ebb8e75d10
Nguyen Tu Anh <nguyentuanh@gapo.com.vn>
2023-04-13 17:54:53 +0700

    Update version 1.6.0

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -43,6 +43,7 @@ function Hello() {
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1 style={{ textAlign: 'center' }}>Ahihi</h1>
+      <h1 style={{ textAlign: 'center' }}>Updated</h1>
       <h3 style={{ textAlign: 'center' }}>
         Version: {process.env.APP_VERSION}
       </h3>
